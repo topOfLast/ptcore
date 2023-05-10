446473652f69befc0718f14af77770dc89c83f62
lidi <lidi@patsnap.com>
2023-05-10 15:57:01 +0800

    remove unused dependencies

--- source/parser/index.ts
+++
@@ -1,7 +0,0 @@
-import {parse} from '@babel/parser';
-import {promises} from 'fs';
-
-export async function getParse(code: string) {
-    const fileData = await promises.readFile(code, {encoding: 'utf8'});
-    return parse(fileData);
-}
